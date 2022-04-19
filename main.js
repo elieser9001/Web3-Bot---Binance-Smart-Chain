8dec0d9607a38a7f537384786f4cd79829f638ca
relieser <102340452+relieser@users.noreply.github.com>
2022-04-19 01:26:07 -0500

    useless function removed

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 // import { relativeTimeThreshold } from 'moment';
 import Panbot from './pancakebot.js'
 
-function toFixed(x) {
-    if (Math.abs(x) < 1.0) {
-      var e = parseInt(x.toString().split('e-')[1]);
-      if (e) {
-          x *= Math.pow(10,e-1);
-          x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
-      }
-    } else {
-      var e = parseInt(x.toString().split('+')[1]);
-      if (e > 20) {
-          e -= 20;
-          x /= Math.pow(10,e);
-          x += (new Array(e+1)).join('0');
-      }
-    }
-    return x;
-  }
-
 async function botEvent(statusType, paramsArray) {
     const stats = this.statusList
 
